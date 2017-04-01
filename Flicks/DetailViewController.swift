7f4e85869cf1027a37440fb0d808f29e460905cb
Alejandro Mendoza <Alejandro_Mendoza@intuit.com>
2017-04-01 14:13:49 -0700

    Added the scroll view.

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -14,6 +14,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
@@ -21,13 +23,27 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let widthSize = scrollView.frame.size.width
+        let heightSize = infoView.frame.origin.y + infoView.frame.size.height
+
+        scrollView.contentSize = CGSize(width: widthSize, height: heightSize)
+        
         let title = movie["title"] as? String
         titleLabel.text = title
         
         let overview = movie["overview"] as? String
         overviewLabel.text = overview
         
+        overviewLabel.sizeToFit()
         
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        
+        if let posterPath = movie["poster_path"] as? String {
+            
+            let imageUrl = URL(string: baseUrl + posterPath)
+            posterImageView.setImageWith(imageUrl!)
+            
+        }
 
         // Do any additional setup after loading the view.
     }
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -75,16 +75,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
-        
         let baseUrl = "https://image.tmdb.org/t/p/w500"
         
-        let imageUrl = URL(string: baseUrl + posterPath)
         
+        if let posterPath = movie["poster_path"] as? String {
+            
+            let imageUrl = URL(string: baseUrl + posterPath)
+            cell.posterView.setImageWith(imageUrl!)
+        
+        }
         
         cell.titleLabal.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWith(imageUrl!)
+        
         
         print("row \(indexPath.row)")
         return cell
